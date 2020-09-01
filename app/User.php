b0834fe0bc677c55c9f5f504fff319cbade74d0b
unknown <anhtuancntt3@gmail.com>
2020-09-01 20:18:20 +0700

    FirstCommit

---
+++ app/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    public function roles(){
+        return $this->hasMany(Role::class,'roleId');
+    }
+
+}
